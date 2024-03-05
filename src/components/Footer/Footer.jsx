ff50ed35e0bebaed5feb8d8d96db5fc00b59cabe
Andriana <andrianasaffo@gmail.com>
2024-03-05 18:56:21 +0000

    Create footer component
    Change href for anchor tags to actual links and apply CSS to format design

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "./footer.css"
+
+export function Footer() {
+  return (
+    <footer>
+      <p>The Village Consult</p>
+      <p>
+        Made with love by{" "}
+        <a href="https://cv-portfolio.onrender.com/">Andriana</a>
+      </p>
+      <p>
+        <a href="https://cv-portfolio.onrender.com/">Blog</a>
+      </p>
+      <p>
+        <a href="https://cv-portfolio.onrender.com/">Instagram</a>
+      </p>
+    </footer>
+  );
+}
