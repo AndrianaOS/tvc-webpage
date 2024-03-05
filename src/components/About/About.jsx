380cd3588fdd274b5d16ce4c6819be4df2fcb6cb
Andriana <andrianasaffo@gmail.com>
2024-03-05 13:17:05 +0000

    Create about component
    This will hold information regarding the purpose of this page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,32 @@
+import "./about.css";
+
+export function About() {
+  return (
+    <div className="about-info">
+      <h3 className="about">About</h3>
+      <p className="about-details">
+        These are the quotes that, for me, encapsulate the purpose of social
+        work. Being in the sector itself, it is difficult within as well as
+        being on the outside. 
+        <br />
+        <br />
+        I can liken it to a massive machine with many
+        parts. Its sufficiency is based on the inclusivity of those that make up
+        the community and those that hold these spaces within said machine.
+        <br />
+        <br />
+        I am a social worker, I love my role, I enjoy the families I work with
+        but most of all I enjoy being of service whether wanted or unwanted.
+        <br />
+        <br />
+        Within this role, I have recognised a massive disconnect between
+        families and social work in understanding our purpose and function 
+        <br />
+        <br />
+        The intention of this blog is to bridge that gap – humanising the
+        service to strengthen the family through a 1-1 consultation service or
+        monthly blog posts.
+      </p>
+    </div>
+  );
+}
