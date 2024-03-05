d202b907265738f5d10b69624062edd4a4a8c9b2
Andriana <andrianasaffo@gmail.com>
2024-03-05 11:58:39 +0000

    Create Hero component
    Hero component includes the function to display the logo. import logo and css files into component for use

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,12 @@
-import logo from "../../../public/TVC-logo.png"
+import "./hero.css";
+import logo from "../../images/TVC-logo.png";
 
-function Hero () {
-    return <section>
-        <img src={logo} alt="website logo" />
+function Hero() {
+  return (
+    <section>
+      <img src={logo} alt="website logo" className="logo"/>
     </section>
+  );
 }
+
+export default Hero;
