70eefa34356f8573860563b40ad0e1130328cf3d
Andriana <andrianasaffo@gmail.com>
2024-03-05 13:18:24 +0000

    Add about component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { Header } from "./components/Header/Header.jsx";
 import { Hero } from "./components/Hero/Hero.jsx";
-import { Quotes } from "./components/Quotes/Quotes";
+import { Quotes } from "./components/Quotes/Quotes.jsx";
+import {About} from "./components/About/About.jsx"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Quotes />
+      <About/>
     </div>
   );
 }
