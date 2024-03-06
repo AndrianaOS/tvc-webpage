f1789d50184f1c81cc747e9d91ad7a6a3f47358b
Andriana <andrianasaffo@gmail.com>
2024-03-06 14:46:02 +0000

    Add classname to section
    This is to enable the use of flex display to utilise whitespace

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import logo from "../../images/TVC-logo.png";
 
 export function Hero() {
   return (
-    <section>
+    <section className="hero-section">
       <img src={logo} alt="website logo" className="logo" />
       <h3>Bridging The Gap, Humanising Social Work</h3>
     </section>
