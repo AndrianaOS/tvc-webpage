a1e2f9bba8a3f063344ccd14ec43cddac613b961
Andriana <andrianasaffo@gmail.com>
2024-03-14 13:46:53 +0000

    Include handling messages
    include success and error use states to show users messages of message is sent successfully or not. Add paragraph to let users know to fill in all sections and to inform them of GDPR

--- src/components/SupportForm/SupportForm.jsx
+++ src/components/SupportForm/SupportForm.jsx
@@ -10,6 +10,8 @@ const formInitialState = {
 
 export function SupportForm() {
   const [formDetails, setFormDetails] = useState(formInitialState);
+  const [handleError, setHandleError] = useState(null);
+  const [handleSuccess, setHandleSuccess] = useState(null);
 
   const form = useRef();
 
@@ -31,12 +33,17 @@ export function SupportForm() {
       .then(
         () => {
           console.log("SUCCESS!");
+          setHandleSuccess("Email successfully sent.");
           setFormDetails(formInitialState);
         },
         (error) => {
           console.log("FAILED...", error.text);
+          setHandleError(
+            "Failed to send email. Please ensure all sections are filled."
+          );
         }
       );
+    e.target.reset();
   };
 
   function handleInputChange(event) {
@@ -49,48 +56,105 @@ export function SupportForm() {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <h3>Contact us for 1-on-1 consultancy services</h3>
-      <section className="support-form">
-        <div className="input-fields" id="name-info">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            className="data_input"
-            placeholder="Enter your name"
-            name="name"
-            value={formDetails.name}
-            onChange={handleInputChange}
-            required
-          />
+      <p className="required-info">
+        <span className="red-asterisk">*</span> Fill in all sections
+      </p>
+      {handleError ? (
+        <div>
+          <p>ALERT: {handleError}</p>
+          <section className="support-form">
+            <div className="input-fields" id="name-info">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className="data_input"
+                placeholder="Enter your name"
+                name="name"
+                value={formDetails.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="input-fields" id="email-info">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="data_input"
+                placeholder="Enter your email"
+                name="email"
+                value={formDetails.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="input-fields" id="message-info">
+              <label htmlFor="message">Your message</label>
+              <textarea
+                type="textbox"
+                id="message"
+                className="data_input"
+                placeholder="What would you like support with"
+                name="message"
+                value={formDetails.message}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </section>
         </div>
-        <div className="input-fields" id="email-info">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="data_input"
-            placeholder="Enter your email"
-            name="email"
-            value={formDetails.email}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="input-fields" id="message-info">
-          <label htmlFor="message">Your message</label>
-          <textarea
-            type="textbox"
-            id="message"
-            className="data_input"
-            placeholder="What would you like support with"
-            name="message"
-            value={formDetails.message}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-      </section>
-
+      ) : (
+        <section>
+          {handleSuccess && <p className="success-msg">{handleSuccess}</p>}
+          <aside className="support-form">
+            <div className="input-fields" id="name-info">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                className="data_input"
+                placeholder="Enter your name"
+                name="name"
+                value={formDetails.name}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="input-fields" id="email-info">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="data_input"
+                placeholder="Enter your email"
+                name="email"
+                value={formDetails.email}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+            <div className="input-fields" id="message-info">
+              <label htmlFor="message">Your message</label>
+              <textarea
+                type="textbox"
+                id="message"
+                className="data_input"
+                placeholder="What would you like support with"
+                name="message"
+                value={formDetails.message}
+                onChange={handleInputChange}
+                required
+              />
+            </div>
+          </aside>
+        </section>
+      )}
+      <p className="required-info">
+        <span className="red-asterisk">**</span> The Village Consult is
+        committed to protecting and respecting your privacy. We will only use
+        your information to provide services you requested from us.
+      </p>
       <button type="submit" className="submit-btn">
         Send email
       </button>
